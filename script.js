16303a67c42d07aac65490db4838ee3eefd0157b
BURS-co <60797909+BURS-co@users.noreply.github.com>
2020-02-20 16:34:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,41 +2,17 @@ var app = angular.module('app', ['ngAnimate'])
 
 app.controller('mainCtrl', function($scope) {
 	$scope.boxes = [{
-		name: 'Friends',
+		name: 'About',
 		image: 'https://source.unsplash.com/uAgLGG1WBd4/900x900'
 	},{
-		name: 'Free',
+		name: 'Iteration 1',
 		image: 'https://source.unsplash.com/Cp-LUHPRpWM/900x900'
 	},{
-		name: 'Explore',
+		name: 'Iteration 2',
 		image: 'https://source.unsplash.com/7BjmDICVloE/900x900'
 	}, {
-		name: 'Vast',
+		name: 'Iteration 3',
 		image: 'https://source.unsplash.com/WLUHO9A_xik/900x900'
-	}, {
-		name: 'Playful',
-		image: 'https://source.unsplash.com/b2-fBVrfx0o/900x900'
-	}, {
-		name: 'Grand',
-		image: 'https://source.unsplash.com/Ixp4YhCKZkI/900x900'
-	}, {
-		name: 'Mist',
-		image: 'https://source.unsplash.com/8BmNurlVR6M/900x900'
-	}, {
-		name: 'Sea',
-		image: 'https://source.unsplash.com/6YqpFWWQsno/900x900'
-	}, {
-		name: 'Reach',
-		image: 'https://source.unsplash.com/zFnk_bTLApo/900x900'
-	}, {
-		name: 'Awe',
-		image: 'https://source.unsplash.com/j4PaE7E2_Ws/900x900'
-	}, {
-		name: 'Surf',
-		image: 'https://source.unsplash.com/uohGiEVhWiQ/900x900'
-	}, {
-		name: 'Thrill',
-		image: 'https://source.unsplash.com/ssrbaKvxaos/900x900'
 	}, ];
 
 	$scope.selected = [];

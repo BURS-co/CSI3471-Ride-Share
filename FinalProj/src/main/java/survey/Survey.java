9de3aac731e5200d6d816655f648c35c945ebb2b
DopeLikeJope <flacom1230@gmail.com>
2020-04-02 00:45:56 -0500

    Made the Survey

--- FinalProj/src/main/java/survey/Survey.java
+++ FinalProj/src/main/java/survey/Survey.java
@@ -1,5 +1,55 @@
 package survey;
 
 public class Survey {
+	private String name;
+	private static String[] questions;
+	private String[] responses;
+	
+	
+	/**
+	 * Initialize the survey
+	 */
+	public Survey(){
+		name = null;
+		questions = new String[5];
+		questions[1] = "How would you rate your ride?";
+		questions[2] = "Were you able to find a ride with ease?";
+		responses = new String[5];
+		for (int i = 0; i < 5; i++) {
+			responses[i] = null;
+		}
+	}
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	/**
+	 * @return the responses
+	 */
+	public String[] getResponses() {
+		return responses;
+	}
+
+
+	/**
+	 * @param responses the responses to set
+	 */
+	public void setResponses(String[] responses) {
+		this.responses = responses;
+	}
 
 }

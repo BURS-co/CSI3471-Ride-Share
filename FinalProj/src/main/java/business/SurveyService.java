1edfdc340b86cf2277abb34391aa47327b71340b
JosephYu1 <Joseph_Yu1@baylor.edu>
2020-04-15 19:55:50 -0500

    SurveyService_skeleton_setup

--- FinalProj/src/main/java/business/SurveyService.java
+++ FinalProj/src/main/java/business/SurveyService.java
@@ -1,5 +1,24 @@
 package business;
 
 public class SurveyService {
-
+    public boolean verifySurvey(String [] list) {
+    	boolean result = true;
+    	if(list.length != 0) {
+    		for(String field : list) {
+    			if(field.isEmpty()) {
+    				result = false;
+    				break;
+    			}
+    		}
+    	}
+    	else {
+    		result = false;
+    	}
+    	
+    	return result;
+    }
+    
+    public void storeSurvey(String [] list) {
+    	//should call the survay database storeSurvey()
+    }
 }

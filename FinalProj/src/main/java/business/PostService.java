fc4c294031555e9d48a164ad41d2fa4f7b46f115
JoshuaHuertas <Jesaixi@gmail.com>
2020-04-15 20:10:40 -0500

    adjusted Validate in PostService

--- FinalProj/src/main/java/business/PostService.java
+++ FinalProj/src/main/java/business/PostService.java
@@ -3,36 +3,55 @@ package business;
 public class PostService {
 
 	public boolean Validate(String[] input) {
+
+		boolean driver = false;
 		
-		if(input.length == 0) {
+		if (input.length == 0) {
 			return false;
 		}
-		
-		for(String i : input) {
-			if(i.length() == 0) {
+
+		for (String i : input) {
+			if (i.length() == 0) {
 				return false;
 			}
 		}
-		
-		
-		
-		//process date string?
-		//or process date object if its been made by presentation layer
-		
-		//Store post if validation is successful
-		StorePost(input);
+
+		if (input.length > 8) {
+			driver = true;
+			
+			if (Integer.valueOf(input[9]) < 1 || Integer.valueOf(input[9]) > 99) {
+				return false;
+			}
+			
+			
+		} else {
+
+		}
+
+		// process date string?
+		// or process date object if its been made by presentation layer
+
+		// Store post if validation is successful
+		StorePost(input, driver);
 		return true;
 	}
 
-	public boolean Validate(String[] input, int capacity) {
-		if(capacity < 1 || capacity > 99) {
-			return false;
+//	public boolean Validate(String[] input, int capacity) {
+//		if(capacity < 1 || capacity > 99) {
+//			return false;
+//		}
+//		
+//		return Validate(input);
+//	}
+
+	public void StorePost(String[] input, boolean driver) {
+
+		if (driver) {
+			// create driver post
+		} else {
+			// create rider post
 		}
-		
-		return Validate(input);
-	}
-	
-	public void StorePost(String[] input) {
-		//should this create a post object to store in the database?
+
+		// should this create a post object to store in the database?
 	}
 }

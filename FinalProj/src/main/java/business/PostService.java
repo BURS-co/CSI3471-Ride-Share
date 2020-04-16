966df1c04469d54dc4b5b324e3999cb5561178a1
JoshuaHuertas <Jesaixi@gmail.com>
2020-04-15 19:53:57 -0500

    basic stuff in post service

--- FinalProj/src/main/java/business/PostService.java
+++ FinalProj/src/main/java/business/PostService.java
@@ -2,4 +2,31 @@ package business;
 
 public class PostService {
 
+	public boolean Validate(String[] input) {
+		
+		if(input.length == 0) {
+			return false;
+		}
+		
+		for(String i : input) {
+			if(i.length() == 0) {
+				return false;
+			}
+		}
+		
+		
+		
+		//process date string?
+		//or process date object if its been made by presentation layer
+		
+		return true;
+	}
+
+	public boolean Validate(String[] input, int capacity) {
+		if(capacity < 1 || capacity > 99) {
+			return false;
+		}
+		
+		return Validate(input);
+	}
 }

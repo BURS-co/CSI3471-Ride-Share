89b6b68dc13252dd926bd151546d10182f82e314
JosephYu1 <Joseph_Yu1@baylor.edu>
2020-04-15 20:58:53 -0500

    Creating_Prospects_class

---
+++ FinalProj/src/main/java/data/post/Prospects.java
@@ -0,0 +1,26 @@
+package data.post;
+
+public class Prospects {
+    private String name;
+    private boolean status;
+    
+    public Prospects(String name) {
+    	this.name = name;
+    }
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

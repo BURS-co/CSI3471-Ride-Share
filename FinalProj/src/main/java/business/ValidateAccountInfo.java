ca47d83ca2866c0ae346bd835ef27e84bd66b9f6
DopeLikeJope <flacom1230@gmail.com>
2020-04-28 22:39:43 -0500

    removed password upper limit

--- FinalProj/src/main/java/business/ValidateAccountInfo.java
+++ FinalProj/src/main/java/business/ValidateAccountInfo.java
@@ -70,7 +70,7 @@ public class ValidateAccountInfo {
 			return false;
 		}
 
-		// validate password
+		// validate password, must comply to regex
 		Pattern p = Pattern.compile("((?=.*[a-z])(?=.*d)(?=.*[!@#$%])(?=.*[A-Z]).{8,})");
 		Matcher m = p.matcher(password);
 

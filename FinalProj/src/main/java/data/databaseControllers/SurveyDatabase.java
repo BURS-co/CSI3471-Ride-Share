e605889851429561d6528bb56b7d8ba78af2ca50
JoshuaHuertas <Jesaixi@gmail.com>
2020-04-28 22:22:45 -0500

    skeleton of survey database load

--- FinalProj/src/main/java/data/databaseControllers/SurveyDatabase.java
+++ FinalProj/src/main/java/data/databaseControllers/SurveyDatabase.java
@@ -1,13 +1,23 @@
 package data.databaseControllers;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.concurrent.locks.ReentrantLock;
 
+import data.post.DriverPost;
+import data.post.Post;
 import data.survey.Survey;
 
 public class SurveyDatabase {
 	// singleton
-	private static SurveyDatabase surveyDatabase = null;// new UserDatabase();
+	private static SurveyDatabase surveyDatabase = null;
 	private static ReentrantLock lock = new ReentrantLock();
 
 	private SurveyDatabase() {
@@ -25,8 +35,28 @@ public class SurveyDatabase {
 
 	private static ArrayList<Survey> surveyData = new ArrayList<Survey>();
 
-	public static void load() {
+	public void load() throws ParseException, IOException{
+		try {
+			BufferedReader loader = new BufferedReader(new FileReader(new File("surveyDatabase.txt")));
 
+			String line = null;
+
+			while ((line = loader.readLine()) != null) {
+
+				String[] split = line.split("-");
+				Survey s = null;
+				for (int i = 0; i < split.length; i++) {
+					 //supply s with info
+				}
+				// Add data
+				surveyData.add(s);
+			}
+			loader.close();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public static void write() {

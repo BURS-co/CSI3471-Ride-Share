935769980dc3a907a99f64cc4822da43cbf9d0ee
Joshua Huertas <Jesaixi@gmail.com>
2020-04-29 16:59:43 -0500

    changes

--- FinalProj/src/main/java/business/Login.java
+++ FinalProj/src/main/java/business/Login.java
@@ -19,34 +19,38 @@ public class Login {
 	 * @return
 	 */
 	public static boolean authenticate(String email, String password) {
-		ArrayList<User> users = UserDatabase.getInstance().getUserData();
-		boolean success = false;
-
-		// search until found.
-		for (int i = 0; i < users.size() && !success; i++) {
-			if (users.get(i).getEmail().toLowerCase().equals(email.toLowerCase())
-					&& users.get(i).getPassword().equals(password)) {
-				// Set user data
+		
+		UserService.getInstance().setCurrentUser(UserDatabase.getInstance().queryDatabase(email, password));
+		
+		return UserService.getInstance().getCurrentUser() != null;
+//		ArrayList<User> users = UserDatabase.getInstance().getUserData();
+//		boolean success = false;
+//
+//		// search until found.
+//		for (int i = 0; i < users.size() && !success; i++) {
+//			if (users.get(i).getEmail().toLowerCase().equals(email.toLowerCase())
+//					&& users.get(i).getPassword().equals(password)) {
+//				 //Set user data
 //				if (users.get(i) instanceof Admin) {
 //					Application.loggedIn = new Admin();
 //				} else {
 //					Application.loggedIn = new User();
 //				}
-
+//
 //				Application.loggedIn.setGradMonth(users.get(i).getGradMonth());
 //				Application.loggedIn.setGradYear(users.get(i).getGradYear());
 //				Application.loggedIn.setEmail(email.toLowerCase());
 //				Application.loggedIn.setPassword(password);
 //				Application.loggedIn.setPhoneNumber(users.get(i).getPhoneNumber());
 //				Application.loggedIn.setUsername(users.get(i).getUsername());
-
-				UserService.getInstance().setCurrentUser(users.get(i));
-
-				success = true;
-			}
-		}
-
-		return success;
+//
+//				UserService.getInstance().setCurrentUser(users.get(i));
+//
+//				success = true;
+//			}
+//		}
+//
+//		return success;
 
 	}
 

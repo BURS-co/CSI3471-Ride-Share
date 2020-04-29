87b323dced44ce04c3b09651409c84235c6964be
Andrew Ammentorp <andrew_ammentorp1@baylor.edu>
2020-04-28 23:21:14 -0500

    User given equals and hash code

--- FinalProj/src/main/java/data/user/User.java
+++ FinalProj/src/main/java/data/user/User.java
@@ -153,4 +153,68 @@ public class User {
 		else
 			this.isAdmin = false;
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((gradMonth == null) ? 0 : gradMonth.hashCode());
+		result = prime * result + ((gradYear == null) ? 0 : gradYear.hashCode());
+		result = prime * result + (isAdmin ? 1231 : 1237);
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (gradMonth == null) {
+			if (other.gradMonth != null)
+				return false;
+		} else if (!gradMonth.equals(other.gradMonth))
+			return false;
+		if (gradYear == null) {
+			if (other.gradYear != null)
+				return false;
+		} else if (!gradYear.equals(other.gradYear))
+			return false;
+		if (isAdmin != other.isAdmin)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (phoneNumber == null) {
+			if (other.phoneNumber != null)
+				return false;
+		} else if (!phoneNumber.equals(other.phoneNumber))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
 }

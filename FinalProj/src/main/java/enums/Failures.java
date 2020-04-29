6e02a07a85c44be39b21d53bdcb8a73e2978ac37
Joshua Huertas <Jesaixi@gmail.com>
2020-04-29 16:08:22 -0500

    part 4

--- FinalProj/src/main/java/enums/Failures.java
+++ FinalProj/src/main/java/enums/Failures.java
@@ -4,6 +4,6 @@ public enum Failures {
 	SUCCESS, emptyField, SurveyField2notANumber, SurveyField3TooLong,
 	         invalidPassengerNumber, SameOriginandDestination, BadDate, PostField8notANumber, 
 	         PostField8NotInRange, invalidName, invalidEmail, emailInUse, invalidPhoneNumber, 
-	         invalidPasswordStandard, passwordMismatch, invalidGraduationDate, invalidPhoneNum, 
-	         passwordsDontMatch, badPassword, invalidGradDate, noMatchingQuery, reachMaxCapacity
+	         invalidPasswordStandard, passwordMismatch, invalidGraduationDate, noMatchingQuery, 
+	         reachMaxCapacity
 }

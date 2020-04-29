6c755fee49b9c6684a4daeeae21c597a5d82b268
JosephYu1 <Joseph_Yu1@baylor.edu>
2020-04-28 23:54:20 -0500

    ReportService_construction

--- FinalProj/src/main/java/business/ReportService.java
+++ FinalProj/src/main/java/business/ReportService.java
@@ -1,5 +1,40 @@
 package business;
 
-public class ReportService {
+import java.util.concurrent.locks.ReentrantLock;
 
+import data.survey.Survey;
+import data.user.Report;
+import enums.Failures;
+
+public class ReportService {
+	// singleton
+		private static ReportService reportService = null;
+		private static ReentrantLock lock = new ReentrantLock();
+		//private SurveyDatabase database;
+
+		private ReportService() {
+			//database = SurveyDatabase.getInstance();
+		}
+
+		public static ReportService getInstance() {
+			if (reportService == null) {
+				lock.lock();
+				if (reportService == null)
+					reportService = new ReportService();
+			}
+
+			return reportService;
+		}
+		
+		public Failures verifyReport(String[] list) {
+			return null;
+		}
+		
+		private Report createReport(String[] list) {
+			return null;
+		}
+		
+		public void storeReport(String[] list) {
+			
+		}
 }

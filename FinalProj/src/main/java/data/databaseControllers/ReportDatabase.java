ac6f36ba248978f7869f2c1cd8c17b5bfe1a8dc3
JosephYu1 <Joseph_Yu1@baylor.edu>
2020-04-28 23:58:37 -0500

    ReportDatabase_construction

--- FinalProj/src/main/java/data/databaseControllers/ReportDatabase.java
+++ FinalProj/src/main/java/data/databaseControllers/ReportDatabase.java
@@ -1,5 +1,43 @@
 package data.databaseControllers;
 
-public class ReportDatabase {
+import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantLock;
 
+import data.user.Report;
+
+public class ReportDatabase {
+	// singleton
+		private static ReportDatabase reportDatabase = null;
+		private static ReentrantLock lock = new ReentrantLock();
+
+		private ReportDatabase() {
+		}
+
+		public static ReportDatabase getInstance() {
+			if (reportDatabase == null) {
+				lock.lock();
+				if (reportDatabase == null)
+					reportDatabase = new ReportDatabase();
+			}
+
+			return reportDatabase;
+		}
+
+		private static ArrayList<Report> reportData = new ArrayList<Report>();
+		
+		public void load() {
+			
+		}
+		
+		public static void write() {
+			
+		}
+		
+		public static void addReport(Report s) {
+			
+		}
+		
+		public static ArrayList<Report> getReportData(){
+			return reportData;
+		}
 }

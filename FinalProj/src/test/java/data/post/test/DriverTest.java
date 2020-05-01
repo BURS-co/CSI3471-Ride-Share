9fd5a7506791f40e82282a0be264e09f711ac82c
Andrew Ammentorp <andrew_ammentorp1@baylor.edu>
2020-05-01 17:47:58 -0500

    driver test

---
+++ FinalProj/src/test/java/data/post/test/DriverTest.java
@@ -0,0 +1,37 @@
+package data.post.test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import data.post.AbstractPost;
+import data.post.Driver;
+import data.post.Prospects;
+public class DriverTest {
+	@Test
+	public void testRiderLimit() {
+		Driver d = new Driver("1");
+		
+		d.setRiderLimit(5);
+		
+		assertEquals((Integer)5,d.getRiderLimit());
+	}
+	@Test
+	public void testProspects() {
+		Driver d = new Driver("1");
+		
+		Prospects p = new Prospects();
+		p.setName("Andrew Ammentorp");
+		p.setStatus(true);
+		
+		d.addRiders(p);
+		
+		assertEquals(d.getRiders().get(0),p);
+		
+		ArrayList<Prospects> ap = new ArrayList<Prospects>();
+		ap.add(p);
+		
+		assertEquals(d.getRiders(),ap);
+	}
+}

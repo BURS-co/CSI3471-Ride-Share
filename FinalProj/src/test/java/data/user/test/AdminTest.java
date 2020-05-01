33a0e8f864c18614133d4dd6973a9cef752ab5cb
Andrew Ammentorp <andrew_ammentorp1@baylor.edu>
2020-05-01 18:57:33 -0500

    added admin test

--- FinalProj/src/test/java/data/user/test/AdminTest.java
+++ FinalProj/src/test/java/data/user/test/AdminTest.java
@@ -1,5 +1,18 @@
 package data.user.test;
 
-public class AdminTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
+import data.user.Admin;
+
+public class AdminTest {
+	@Test
+	public void testToString() {
+		Admin a = new Admin();
+		//test if gets true for being an admin
+		assertEquals(a.toString(),"null" + ",,," + "null" + ",,," + "null" + ",,," + "null" + ",,," + "null" + ",,," + "null"
+				+ ",,,true,,," + "false" + ",,," + "false" + "\n");
+		
+	}
 }

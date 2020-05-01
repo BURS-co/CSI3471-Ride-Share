f4ec1a53b8cb30f8e3e8b876c2e018b34b87cdc2
Andrew Ammentorp <andrew_ammentorp1@baylor.edu>
2020-05-01 16:53:01 -0500

    Added post database test

---
+++ FinalProj/src/test/java/business/test/PostDatabaseTest.java
@@ -0,0 +1,18 @@
+package business.test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import data.databaseControllers.PostDatabase;
+
+public class PostDatabaseTest {
+	@Test
+	public void testGetInstance() {
+		//testing singleton
+		PostDatabase a = PostDatabase.getInstance();
+		PostDatabase b = PostDatabase.getInstance();
+		
+		assertEquals(a,b);
+		assertSame(a,b);
+	}
+}
